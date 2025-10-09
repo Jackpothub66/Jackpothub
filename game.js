770df715da9ec0a2260a78f64ac83e0451938e08
Jackpothub66 <jackpothub66@gmail.com>
2025-10-09 22:40:06 +0530

    Delete game.js

--- game.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const gameSchema = new mongoose.Schema({
-title: { type: String, required: true },
-type: { type: String, required: true }, // skill, lucky, tournament
-bet: { type: Number, required: true },
-img: { type: String, required: true }
-});
-module.exports = mongoose.model('Game', gameSchema);
